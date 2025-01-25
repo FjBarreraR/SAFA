a81401fe88ecbfb445e1af5dc6b681515285d6c1
FjBarreraR <franbarreraroman0@gmail.com>
2025-01-25 13:26:57 +0100

    commit 23

--- BBDD/2_trimestre/ejercicios 3.sql
+++ BBDD/2_trimestre/ejercicios 3.sql
@@ -1,1 +1,50 @@
 /* Ejercicio 1 */
+select titulo , char_length(titulo) longitud_caracteres, length(titulo) longitud_bytes
+from libros;
+
+/* Ejercicio 2 */
+select upper(titulo)
+from libros;
+
+/* Ejercicio 3 */
+select substr(titulo, 1, 10)
+from libros;
+
+/* Ejercicio 4 */
+select concat(titulo, " ", "-", " ",autor)
+from libros;
+
+/* Ejercicio 5 */
+select round(precio, 1)
+from libros;
+
+/* Ejercicio 6 */
+select ltrim(titulo)
+from libros;
+
+/* Ejercicio 7 */
+select replace(titulo, "El", "La")
+from libros;
+
+/* Ejercicio 8 */
+select abs(precio - 20)
+from libros;
+
+/* Ejercicio 9 */
+SET lc_time_names = 'es_ES';
+select date_format(fecha_publicacion,'%d/%m/%Y')
+from libros;
+
+/* Ejercicio 10 */
+select extract(year from fecha_publicacion)
+from libros;
+
+/* Ejercicio 11 */
+select titulo
+from libros
+having titulo = "%de%";
+
+/* Ejercicio 12 */
+
+
+
---
+++ ED/2_trimestre/practica4/src/tipoPunto3D/propiedadesBasicas.java
@@ -0,0 +1,10 @@
+package tipoPunto3D;
+
+public interface propiedadesBasicas {
+    public Integer getX();
+    public void setX(Integer x);
+    public Integer getY();
+    public void setY(Integer y);
+    public Integer getZ();
+    public void setZ(Integer z);
+}
