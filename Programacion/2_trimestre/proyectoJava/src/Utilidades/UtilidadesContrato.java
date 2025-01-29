7918793aabd69e43f4057c56a3a5e14398ac78b5
Fran <franbarreraroman0@gmail.com>
2025-01-29 19:42:13 +0100

    commit 26

--- Programacion/2_trimestre/proyectoJava/src/Utilidades/UtilidadesContrato.java
+++ Programacion/2_trimestre/proyectoJava/src/Utilidades/UtilidadesContrato.java
@@ -1,4 +1,28 @@
 package Utilidades;
 
+import Modelos.Contrato;
+import Modelos.TipoContrato;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class UtilidadesContrato {
+    public Map<TipoContrato, Integer> getNumContratosPorTipo (List<Contrato> contratos) {
+        Map<TipoContrato, Integer> NumContratosPorTipo = new HashMap<>();
+        for (Contrato cont : contratos) {
+            //MAPA VACIO
+            if(!NumContratosPorTipo.containsKey(cont.getTipoContrato())) {
+                NumContratosPorTipo.put(cont.getTipoContrato(), 1);
+            }else{
+                NumContratosPorTipo.put(cont.getTipoContrato(), NumContratosPorTipo.get(cont.getTipoContrato()) + 1);
+            }
+        }
+        return NumContratosPorTipo;
+    }
+
+    public Map<TipoContrato, List<Contrato> getListContratosPorTipo((List<Contrato> contratos){
+
+    }
 }
--- Programacion/2_trimestre/proyectoJava/src/Utilidades/UtilidadesEmpleado.java
+++ Programacion/2_trimestre/proyectoJava/src/Utilidades/UtilidadesEmpleado.java
@@ -1,4 +1,11 @@
 package Utilidades;
 
+import Modelos.Empleado;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class UtilidadesEmpleado {
+
 }
