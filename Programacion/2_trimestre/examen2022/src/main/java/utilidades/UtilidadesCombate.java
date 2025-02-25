7e1f46819eb9d3606a78bae0bb26eda1be74bf88
Fran <franbarreraroman0@gmail.com>
2025-02-25 20:42:31 +0100

    commit 53

--- Programacion/2_trimestre/examen2022/src/main/java/utilidades/UtilidadesCombate.java
+++ Programacion/2_trimestre/examen2022/src/main/java/utilidades/UtilidadesCombate.java
@@ -1,8 +1,10 @@
 package utilidades;
 
 import org.example.modelos.Entrenador;
+import org.example.modelos.LineaEvolutiva;
 import org.example.modelos.Pokemon;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
@@ -10,4 +12,27 @@ public class UtilidadesCombate {
     public Map<Entrenador, Integer> repartirPokemon(List<Entrenador> entrenadores, List<Pokemon> pokemon){
 
     }
+
+    public void subirAlNivel(Pokemon pokemon, Integer nivel){
+        pokemon.setNivel(nivel);
+        pokemon.getStats().setAt(pokemon.getStats().getAt() + (nivel * 2));
+        pokemon.getStats().setDf(pokemon.getStats().getDf() + (nivel * 2));
+        pokemon.getStats().setSpa(pokemon.getStats().getSpa() + (nivel * 2));
+        pokemon.getStats().setSpdf(pokemon.getStats().getSpdf() + (nivel * 2));
+        pokemon.getStats().setSpd(pokemon.getStats().getSpd() + (nivel * 2));
+        pokemon.getStats().setPs(pokemon.getStats().getPs() + (nivel * 2));
+    }
+
+    public boolean puedeEvolucionar(Pokemon pokemon){
+        Integer nivel = pokemon.getNivel();
+        Integer posicion = pokemon.getLinenaEvolutiva()
+                .stream()
+                .filter(p->p.getPokemon().getLinenaEvolutiva().equals(pokemon.getLinenaEvolutiva()))
+                .findFirst().get().getOrden();
+
+        Integer maxOden = pokemon.getLinenaEvolutiva().stream().max(Comparator.comparing(LineaEvolutiva::getOrden)).get().getOrden();
+
+        return 
+    }
+
 }
