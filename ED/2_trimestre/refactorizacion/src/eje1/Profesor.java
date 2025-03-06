e26b73e1c65dcf91514c04946268841e67ac72ec
Fran <franbarreraroman0@gmail.com>
2025-03-06 16:05:56 +0100

    commit 60

---
+++ ED/2_trimestre/refactorizacion/src/eje1/Profesor.java
@@ -0,0 +1,29 @@
+package eje1;
+
+import java.util.List;
+
+public class Profesor extends Persona {
+    String str;
+    int edad;
+    String numeroDeTelefono;
+    List<Prestamo> prestamos;
+
+    public Profesor(String numeroDeTelefono) {
+        super(numeroDeTelefono);
+    }
+
+    public void printInformacionPersonal() {
+        System.out.println("Nombre: " + str);
+        System.out.println("Edad: " + edad);
+        System.out.println("Teléfono: " + numeroDeTelefono);
+    }
+
+    public void printTodaLaInformacion() {
+        System.out.println("Nombre: " + str);
+        System.out.println("Edad: " + edad);
+        System.out.println("Teléfono: " + this.numeroDeTelefono);
+        for (Prestamo prestamo : prestamos) {
+            System.out.println(prestamo);
+        }
+    }
+}
